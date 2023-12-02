47cb8fcc2b1c2d5a2045aee75696e2e73487bdfb
bipo38 <bipoben7@gmail.com>
2023-12-02 23:49:01 +0100

    restaurants

--- app/src/main/java/com/asiergilaber/foodapp/restaurant/presentation/RestaurantScreen.kt
+++ app/src/main/java/com/asiergilaber/foodapp/restaurant/presentation/RestaurantScreen.kt
@@ -2,36 +2,44 @@ package com.asiergilaber.foodapp.restaurant.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.ExitToApp
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
+import com.asiergilaber.foodapp.ui.theme.YellowMain
+
 
 @Composable
 fun RestaurantsScreen(restaurantViewModel: RestaurantViewModel , modifier: Modifier = Modifier) {
@@ -41,32 +49,109 @@ fun RestaurantsScreen(restaurantViewModel: RestaurantViewModel , modifier: Modif
         restaurantViewModel.getRestaurant(1)
     }
 
-    Box(){
-        AsyncImage(
-            model = restaurant?.coverUrl ?: "",
-            contentDescription = "Restaurant image",
-            contentScale = ContentScale.Crop,
-            modifier = modifier
-                .fillMaxWidth()
-                .height(250.dp)
-                .alpha(0.8f),
+    Column {
+        Box() {
+            AsyncImage(
+                model = restaurant?.coverUrl ?: "",
+                contentDescription = "Restaurant image",
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+                    .alpha(0.7f),
 
-            )
+                )
+            Column {
+                Row(modifier = Modifier.padding(10.dp)) {
+                    IconButton(modifier = Modifier
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(2.dp),
+                        onClick = { restaurantViewModel.logout() }) {
+                        Icon(Icons.Outlined.ExitToApp, "Logout")
+                    }
 
-        Row(modifier = Modifier.padding(10.dp)) {
-                IconButton(modifier = Modifier
-                    .background(Color.White)
-                    .padding(10.dp)
-                    .clip(CircleShape) ,
-                    onClick = { restaurantViewModel.logout() }) {
-                    Icon(Icons.Outlined.ArrowBack , "Logout")
+                    IconButton(modifier = Modifier
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(2.dp),
+                        onClick = { }) {
+                        Icon(Icons.Outlined.FavoriteBorder, "Logout")
+                    }
+                    IconButton(modifier = Modifier
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(2.dp),
+                        onClick = {  }) {
+                        Icon(Icons.Outlined.LocationOn, "Logout")
+                    }
+
+
                 }
+                Text(
+                    text = restaurant?.name ?: "",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    style = MaterialTheme.typography.headlineLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                )
+
+            }
         }
 
+        Box( modifier = modifier
+            .padding(8.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())){
+            val dishes =
+                arrayOf(restaurant?.dishes, restaurant?.dishes).flatMap { it ?: emptyList() }
+            
 
-        
+            
+            Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
 
+                for (dish in dishes) {
+                    Row(horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.fillMaxSize(), verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        AsyncImage(model = dish.coverUrl,
+                            contentDescription = dish.name ?: "",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(100.dp, 100.dp)
+                                .clip(MaterialTheme.shapes.medium)
+                        )
+                        Spacer(modifier = Modifier.padding(horizontal = 4.dp))
+
+                        Column(modifier = Modifier.widthIn(0.dp,200.dp)) {
+                            Text(text = dish.name,
+                                style = MaterialTheme.typography.titleSmall.copy(
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
+                            ))
+                            Spacer(modifier = Modifier.padding(top = 2.dp))
+                            Text(text = dish.description,
+                                style = MaterialTheme.typography.bodySmall.copy(
+                                fontWeight = FontWeight.Medium,
+                                color = Color.Gray
+                            ))
+                            Spacer(modifier = Modifier.padding(top = 2.dp))
+                            Text(text = "12,00€")
+                        }
+
+                        IconButton(onClick = { /*TODO*/ } , modifier = Modifier
+                            .clip(CircleShape)
+                            .background(YellowMain)) {
+                            Icon(Icons.Outlined.Add, "Add" , tint = Color.White)
+                        }
+
+                    }
+
+                }
+            }
+
+        }
     }
-
-
 }
